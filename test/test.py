d85217869329b9bf6fb3f793275d7d8f98d16629
daviana putra <106154800+motebaya@users.noreply.github.com>
2022-10-03 05:55:12 +0700

    create test file

---
+++ test/test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from lib import Base, MangaSearch
+import asyncio
+
+async def test_all():
+    module = MangaSearch.Mangasearch() # call class
+    module.host = module.host.format('manhwaland.guru') # set host
+    print(module.host)
+    search = await module.search('lea') # search result list type(map)
+    search = list(search)
+    print(search)
+    print()
+    chapter = await s.get_chapter(search[0]['a']) # chapter list
+    chapter = list(chapter)
+    print(chapter)
+    print()
+    images = await s.get_images(chapter[0]['a']) # tuple output with title & images list
+    # or
+    title, images_list = images # unpack tuple values
+    print(images)
+
+if __name__=='__main__':
+    asyncio.run(test_all())
